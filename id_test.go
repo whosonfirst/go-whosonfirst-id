112ba9b090eb81d8f655add2b8f894172dbf7bf7
thisisaaronland <devnull@localhost>
2019-12-16 13:09:17 -0800

    change everything, use a custom Provider interface

--- id_test.go
+++ id_test.go
@@ -9,13 +9,13 @@ import (
 func TestNextInt(t *testing.T) {
 
 	ctx := context.Background()
-	cl, err := NewIdClient(ctx)
+	pr, err := NewProvider(ctx)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	id, err := cl.NextInt()
+	id, err := pr.NewID()
 
 	if err != nil {
 		t.Fatal(err)
@@ -29,13 +29,13 @@ func TestNextIntWithURI(t *testing.T) {
 	uri := "artisanal:///?client=brooklynintegers%3A%2F%2F&minimum=5&pool=memory%3A%2F%2F"
 	
 	ctx := context.Background()
-	cl, err := NewIdClientWithURI(ctx, uri)
+	pr, err := NewProviderWithURI(ctx, uri)
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	id, err := cl.NextInt()
+	id, err := pr.NewID()
 
 	if err != nil {
 		t.Fatal(err)
